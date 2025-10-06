0be0d38426f281a1a931af4e72a0cef6e973c808
mayuribhujbal16 <mayuribhujbal9893@gmail.com>
2025-10-06 14:50:34 +0530

    learned basic c nd datatype: char

---
+++ datatype_char.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	/*
+	syntax: declaration datatype;
+	       datatype varname;
+	       char ch1,ch2,ch3;
+	       int n1,n2=100(initialization);
+	       float x,y;
+	       
+	 
+	*/
+
+char ch1='A' , ch2, ch3;
+printf("ch1=%c\n",ch1);
+ch2='4';
+printf("ch2=%c\n",ch2);
+ch3='@';
+printf("ch3=%c\n",ch3);
+
+printf("ch2=%c\n",ch3); //here o/p: is ch2=@ 
+printf("ch3=%c\n");    // here o/p: garbage (any random value). 
+	
+	
+}
---
+++ print hello.c
@@ -0,0 +1,13 @@
+// structure of c lang.
+#include<stdio.h>
+int main()
+{
+	printf("Hey BEautifulll\n");  // use '\n' for new line
+	printf("How r uhh \nMayura \n");
+	printf("Wel\\ncome \n");
+	printf("Fortune\tcloud \n");  //'\t' : used for space 
+	printf("");
+} 
+
+
+
